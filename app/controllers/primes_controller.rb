756c06d216da2dcd00e5cd788fd5b8c66957f287
Alex Braha Stoll <alexbrahastoll@gmail.com>
2016-09-17 15:45:48 -0300

    Adds primes Rust library and implements RustyPrime as a wrapper to it by using the ffi gem.

--- app/controllers/primes_controller.rb
+++ app/controllers/primes_controller.rb
@@ -1,6 +1,7 @@
 class PrimesController < ApplicationController
   def index
-    @primes = ''
-    @primes = RustyPrime.new.first(params[:primes_count]) if params[:primes_count].present?
+    if params[:primes_count].present?
+      @primes = RustyPrime.list_primes(params[:primes_count].to_i)
+    end
   end
 end
--- app/models/rusty_prime.rb
+++ app/models/rusty_prime.rb
@@ -1,5 +1,16 @@
-class RustyPrime
-  def first(primes_count)
-    "Found #{primes_count} primes."
+module RustyPrime
+  extend FFI::Library
+
+  ffi_lib 'lib/cargo/primes/target/release/libprimes.dylib'
+  attach_function :nth_prime, [:int], :int
+  attach_function :list_primes_up_to_nth, [:int], :string
+  attach_function :deallocate_c_str, [:pointer], :void
+
+  def self.list_primes(nth)
+    unsafe_list = list_primes_up_to_nth(nth)
+    safe_list = unsafe_list.clone
+    deallocate_c_str(FFI::MemoryPointer.from_string(unsafe_list))
+
+    safe_list
   end
 end
