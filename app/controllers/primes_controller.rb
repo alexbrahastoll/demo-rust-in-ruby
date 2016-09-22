01f6a003eb5ce9d373a714092c854c6f7bbcec37
Alex Braha Stoll <alexbrahastoll@gmail.com>
2016-09-22 02:06:42 -0300

    Changes primes#index to choose prime discovery implementation based on the value of PRIMES_IMPL.

--- app/controllers/primes_controller.rb
+++ app/controllers/primes_controller.rb
@@ -1,7 +1,8 @@
 class PrimesController < ApplicationController
   def index
     if params[:primes_count].present?
-      @primes = RustyPrime.list_primes(params[:primes_count].to_i)
+      impl = ENV.fetch('PRIMES_IMPL', 'RustyPrime').constantize
+      @primes = impl.send(:list_primes, params[:primes_count].to_i)
     end
   end
 end
